a19e3d5fe41cd1187b444781e30f4d2b8b547cf7
Ivan Andreev <fc97@mail.ru>
2023-02-24 13:24:24 +0400

    замена int на long

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int[] sales = {112, 23, 31, 47, 59, 7, 115};
+        long[] sales = {112, 23, 31, 47, 59, 7, 115};
         SalesManager manager = new SalesManager(sales);
-        int max = manager.max();
+        long max = manager.max();
         System.out.println("Максимум продаж в день - " + max);
     }
 }
